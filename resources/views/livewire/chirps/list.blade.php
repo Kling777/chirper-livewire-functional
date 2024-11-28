9bc33a75ef881a9af60c0dec372202a435db3b4b
Kling777 <2good4u991@gmail.com>
2024-11-28 15:24:35 +0700

    [Update] delete feature

--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -23,6 +23,14 @@ $edit = function (Chirp $chirp) {
 
     $this->getChirps();
 };
+
+$delete = function (Chirp $chirp) {
+    $this->authorize('delete', $chirp);
+
+    $chirp->delete();
+
+    $this->getChirps();
+};
 ?>
 
 <div>
@@ -58,6 +66,10 @@ $edit = function (Chirp $chirp) {
                                 <x-slot name="content">
                                     <x-dropdown-link wire:click="edit({{ $chirp->id }})">
                                         {{ __('Edit') }}
+                                    </x-dropdown-link>
+                                    <x-dropdown-link wire:click="delete({{ $chirp->id }})"
+                                        wire:confirm="Are you sure to delete this chirp?">
+                                        {{ __('Delete') }}
                                     </x-dropdown-link>
                                 </x-slot>
                             </x-dropdown>
